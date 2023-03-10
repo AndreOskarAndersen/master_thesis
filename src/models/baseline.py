189cf1b67042046dd961ef8277c9ac59264fcd94
keenbitgithub <keenbitinfo@gmail.com>
2023-03-10 09:09:34 +0100

    Baseline documentation

--- src/models/baseline.py
+++ src/models/baseline.py
@@ -4,6 +4,21 @@ import torch.nn as nn
 class Baseline(nn.Module):
     def __init__(self, num_keypoints: int, kernel_size: int, stride: int):
         super(Baseline, self).__init__()
+        """
+        Implementation of Baseline model, consisting of a single 3D-convolution.
+        
+        Parameters
+        -----
+        num_keypoints : int
+            Maximum numbe of keypoints
+            
+        kernel_size : int
+            Size of the kernel used by the convolution.
+            
+        stride : int
+            Stride of the kernel used by the convolution.
+        """
+        
         self.conv = nn.Conv3d(
             in_channels=num_keypoints,
             out_channels=num_keypoints,
@@ -12,14 +27,31 @@ class Baseline(nn.Module):
             padding="same"
         )
         
-    def forward(self, x):
-        return self.conv(x)
+    def forward(self, p_noisy: torch.Tensor):
+        """
+        Applies the baseline-model on a set of poses.
+        
+        Parameters
+        ----------
+        p_noisy : torch.Tensor
+            Set of poses to apply the baseline-model on.
+            
+        Returns
+        -------
+        pred : torch.Tensor
+            Set of processed poses.
+        """
+        
+        pred = self.conv(p_noisy)
+        
+        return pred
 
 if __name__ == "__main__":
     """
     Example on using the Baseline Implementation
     """
     
+    # Making data
     num_frames = 100
     num_keypoints = 16
     frame_height = 8
