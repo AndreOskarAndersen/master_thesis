5b07474e68290bfb1ab8459bca9b1380301a113c
KeenbitGitHub <keenbitinfo@gmail.com>
2023-03-15 12:08:03 +0100

    Some deciwatch-refactorization

--- src/models/test.py
+++ src/models/test.py
@@ -2,39 +2,36 @@ import numpy as np
 import torch
 from torch import nn
 
-def _get_masks(seq_len, sample_rate=10):        
-    assert (seq_len - 1) % sample_rate == 0, "seq_len + 1 has to be divisible by sample_rate"
+def _get_masks(num_frames, sample_rate=10):        
+    assert (num_frames - 1) % sample_rate == 0, "num_frames + 1 has to be divisible by sample_rate"
 
-    encoder_mask = torch.zeros(seq_len, dtype=bool)
+    encoder_mask = torch.zeros(num_frames, dtype=bool)
     encoder_mask[::sample_rate] = 1
 
-    decoder_mask = torch.zeros(seq_len, dtype=bool)
+    decoder_mask = torch.zeros(num_frames, dtype=bool)
 
     return encoder_mask, decoder_mask
 
 class PositionEmbeddingSine_1D(nn.Module):
-    def __init__(self, num_pos_feats):
+    def __init__(self, d_model):
         super(PositionEmbeddingSine_1D, self).__init__()
         
-        self.num_pos_feats = num_pos_feats
-        self.temperature = 10000
-        self.scale = 2 * np.pi
+        self.d_model = d_model
+        self.denum = 10000**(2 * np.arange(0, self.d_model)/self.d_model)
 
-    def forward(self, batch_size, seq_len):
-        position = torch.arange(seq_len).unsqueeze(0).repeat(batch_size, 1)
-        position = position / (position[:, -1:] + 1e-6) * self.scale
+    def forward(self, batch_size, num_frames):
+        pos = torch.arange(num_frames).unsqueeze(0).repeat(batch_size, 1)
+        pos = pos / (pos[:, -1:] + 1e-6) * 2 * np.pi
 
-        dim_t = self.temperature**(2 * (torch.div(torch.arange(self.num_pos_feats), 1, rounding_mode='trunc')) / self.num_pos_feats)
-
-        e_pos = torch.zeros(batch_size, seq_len, self.num_pos_feats * 2)
-        e_pos[:, :, 0::2] = torch.sin(position[:, :, None] / dim_t)
-        e_pos[:, :, 1::2] = torch.cos(position[:, :, None] / dim_t)
+        e_pos = torch.zeros(batch_size, num_frames, self.d_model * 2)
+        e_pos[:, :, 0::2] = torch.sin(pos[:, :, None] / self.denum)
+        e_pos[:, :, 1::2] = torch.cos(pos[:, :, None] / self.denum)
         e_pos = e_pos.permute(1, 0, 2)
 
         return e_pos
     
 class _DenoiseNet(nn.Module):
-    def __init__(self, encoder_hidden_dim, dim_feedforward, num_encoder_layers, joints_dim, dropout):
+    def __init__(self, encoder_hidden_dim, dim_feedforward, num_encoder_layers, joints_dim, nheads, dropout):
         super(_DenoiseNet, self).__init__()
         
         self.encoder_embed = nn.Linear(joints_dim, encoder_hidden_dim)
@@ -51,15 +48,15 @@ class _DenoiseNet(nn.Module):
         
         self.encoder_joints_embed = nn.Linear(encoder_hidden_dim, joints_dim)
         
-    def forward(self, input_seq, e_pos, encoder_mask):
-        trans_src = self.encoder_embed(input_seq) + e_pos
+    def forward(self, video_seq, e_pos, encoder_mask):
+        trans_src = self.encoder_embed(video_seq) + e_pos
         mem = self.encoder(trans_src, mask=torch.eye(trans_src.shape[0], dtype=bool), src_key_padding_mask=encoder_mask)
-        p_clean = self.encoder_joints_embed(mem) + input_seq
+        p_clean = self.encoder_joints_embed(mem) + video_seq
         
         return p_clean, mem
     
 class _RecoverNet(nn.Module):
-    def __init__(self, keypoints_numel, decoder_hidden_dim, dim_feedforward, num_decoder_layers, dropout, sample_rate):
+    def __init__(self, decoder_hidden_dim, dim_feedforward, num_decoder_layers, keypoints_numel, nheads, dropout, sample_rate):
         super(_RecoverNet, self).__init__()
         
         self.sample_rate = sample_rate
@@ -68,7 +65,12 @@ class _RecoverNet(nn.Module):
         
         decoder_norm = nn.LayerNorm(decoder_hidden_dim)
         self.decoder = nn.TransformerDecoder(
-            nn.TransformerDecoderLayer(d_model=decoder_hidden_dim, nhead=nheads, dim_feedforward=dim_feedforward, dropout=dropout),
+            nn.TransformerDecoderLayer(
+                d_model=decoder_hidden_dim, 
+                nhead=nheads, 
+                dim_feedforward=dim_feedforward, 
+                dropout=dropout
+            ),
             num_layers=num_decoder_layers,
             norm=decoder_norm
         )
@@ -80,7 +82,7 @@ class _RecoverNet(nn.Module):
         interpolation_clone = interpolation.clone()
         
         trans_tgt = self.decoder_embed(interpolation.permute(1, 2, 0)).permute(2, 0, 1)
-        output = self.decoder(tgt=trans_tgt + e_pos, memory=f_clean + e_pos, tgt_key_padding_mask=decoder_mask, memory_key_padding_mask=encoder_mask)
+        output = self.decoder(tgt=trans_tgt + e_pos, memory=f_clean, tgt_key_padding_mask=decoder_mask, memory_key_padding_mask=encoder_mask)
         joints = self.decoder_joints_embed(output) + interpolation_clone
         
         return joints
@@ -93,20 +95,20 @@ class DeciWatch(nn.Module):
         self.sample_rate = sample_rate
 
         self.e_pos = PositionEmbeddingSine_1D(self.pos_embed_dim // 2)
-        self.denoise_net = _DenoiseNet(encoder_hidden_dim, dim_feedforward, num_encoder_layers, keypoints_numel, dropout)
-        self.recover_net = _RecoverNet(keypoints_numel, decoder_hidden_dim, dim_feedforward, num_decoder_layers, dropout, sample_rate)
+        self.denoise_net = _DenoiseNet(encoder_hidden_dim, dim_feedforward, num_encoder_layers, keypoints_numel, nheads, dropout)
+        self.recover_net = _RecoverNet(decoder_hidden_dim, dim_feedforward, num_decoder_layers, keypoints_numel, nheads, dropout, sample_rate)
 
     def forward(self, video_sequence):
-        batch_size, seq_len, keypoints_numel = video_sequence.shape
+        batch_size, num_frames, keypoints_numel = video_sequence.shape
         
-        encoder_mask, decoder_mask = _get_masks(seq_len, sample_rate=self.sample_rate)
+        encoder_mask, decoder_mask = _get_masks(num_frames, sample_rate=self.sample_rate)
         
         video_sequence = (video_sequence.permute(0, 2, 1) * encoder_mask.int()).permute(2, 0, 1)
         
-        e_pos = self.e_pos(batch_size, seq_len)
+        e_pos = self.e_pos(batch_size, num_frames)
         p_clean, f_clean = self.denoise_net(video_sequence, e_pos, encoder_mask.unsqueeze(0).repeat(batch_size, 1))
         p_estimated = self.recover_net(p_clean, f_clean, encoder_mask.unsqueeze(0).repeat(batch_size, 1), decoder_mask.unsqueeze(0).repeat(batch_size, 1), e_pos)
-        p_estimated = p_estimated.permute(1, 0, 2).reshape(batch_size, seq_len, keypoints_numel)
+        p_estimated = p_estimated.permute(1, 0, 2).reshape(batch_size, num_frames, keypoints_numel)
 
         return p_estimated
 
