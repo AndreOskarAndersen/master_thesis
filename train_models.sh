9d78e5ce5a51bfcb9bb55a4dcace05bb2b5a2d8b
wpr684 <wpr684@hendrixgate02fl.unicph.domain>
2023-04-01 11:54:24 +0200

    train_models.sh

---
+++ train_models.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+#SBATCH -p gpu --gres=gpu:titanrtx:2
+#SBATCH --job-name=wpr684
+#SBATCH --cpus-per-task=4
+#SBATCH --time=4-02:00:00
+#SBATCH --array 0-2%3
+
+# Loading modules
+module load python/3.9.9
+module load cuda/11.8
+
+export LD_LIBRARY_PATH=/opt/software/cuda/11.8/lib64:$LD_LIBRARY_PATH
+export PATH=/opt/software/cuda/11.8/bin:$PATH
+
+# Changing directory to master_thesis
+cd master_thesis
+
+# Loading virtual environment
+python3 -m venv venv
+source ./venv/bin/activate
+
+# Installing libraries
+/home/wpr684/master_thesis/venv/bin/python3 -m pip --no-cache-dir install --upgrade pip
+/home/wpr684/master_thesis/venv/bin/python3 -m pip --no-cache-dir install torch torchvision torchaudio --index-url https://download.pytorch.org/whl/cu118
+/home/wpr684/master_thesis/venv/bin/python3 -m pip --no-cache-dir install -r requirements.txt
+
+hostname
+echo $CUDA_VISIBLE_DEVICES
+
+# Running script
+cd ./src/models
+python3 -m train_models ${SLURM_ARRAY_TASK_ID}
