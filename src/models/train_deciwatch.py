764799c645af6e078b55954dc60183714852259e
keenbitgithub <keenbitinfo@gmail.com>
2023-04-29 20:29:18 +0200

    bugfix

--- src/models/train_deciwatch.py
+++ src/models/train_deciwatch.py
@@ -70,6 +70,7 @@ if __name__ == "__main__":
     device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
     
     # Collecting model params
+    deciwatch_params["device"] = device
     model = DeciWatch(**deciwatch_params).to(device)
     
     # Getting name of model type
