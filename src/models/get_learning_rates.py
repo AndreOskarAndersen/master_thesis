b63477db138c7e9d08f6d50d7aa0faab35b66e3b
wpr684 <wpr684@hendrixgate01fl.unicph.domain>
2023-05-09 23:02:02 +0200

    get_learning_rates

--- src/models/get_learning_rates.py
+++ src/models/get_learning_rates.py
@@ -1,23 +1,37 @@
 import os
 import json
 import torch
-from config import pretrained_models_path
+from config import overall_models_dir
+from tqdm.auto import tqdm
 
 def main():
     overall_dict = {}
     
-    for dir in pretrained_models_path:
-        for model in os.listdir(pretrained_models_path + dir):
+    for dir in tqdm(os.listdir(overall_models_dir), desc="dir", leave=False):
+        if dir == ".gitignore":
+            continue
+
+        for model in tqdm(os.listdir(overall_models_dir + dir), desc="model", leave=False):
             learning_rates = []
+
+            gen = os.walk(overall_models_dir + dir + "/" + model)
             
-            for epoch in os.walk(pretrained_models_path + dir + "/" + model):
-                scheduler = torch.load(pretrained_models_path + dir + "/" + model + "/" + epoch + "/scheduler.pth", map_location=torch.device("cpu"))
-                learning_rates.append(scheduler.state_dict()["_last_lr"])
+            for _, epochs, _ in tqdm(gen, desc="epoch", leave=False):
                 
+                epochs = sorted(list(map(lambda x: int(x), epochs)))
+
+                for epoch in sorted(epochs):
+                    scheduler = torch.load(overall_models_dir + dir + "/" + model + "/" + str(epoch) + "/scheduler.pth", map_location=torch.device("cpu"))
+                    
+                    if "_last_lr" in scheduler.state_dict():
+                        learning_rates.append((epoch, scheduler.state_dict()["_last_lr"]))                
+                    else:
+                        learning_rates.append((epoch, -1))
+
             overall_dict[model] = learning_rates
             
     with open("./learning_rates.json", "w") as f:
-        json.dump(overall_dict, f)
+        json.dump(overall_dict, f, indent=4)
 
 if __name__ == "__main__":
     main()
